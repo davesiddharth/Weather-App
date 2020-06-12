dccac73864a4d1a3a73c2e567ad695c40bc1fc0d
Charrcoal <58637795+Charrcoal@users.noreply.github.com>
2020-06-11 21:40:38 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,37 +1,29 @@
-#Importing dependencies
-from flask import Flask, jsonify,render_template
+from flask import Flask, jsonify, render_template
 from flask_pymongo import PyMongo
 import pymongo
-from config1 import username, password
-import os
 from bson import json_util
 import json
-
-
 # Create an instance of Flask
 app = Flask(__name__)
-
-conn="mongodb+srv://"+username+":"+password+"@cluster1-oinat.mongodb.net/weather_data?retryWrites=true&w=majority"
-
+conn="mongodb+srv://connect_user:Project_2@cluster1-oinat.mongodb.net/weather_data?retryWrites=true&w=majority"
 # Use PyMongo to establish Mongo connection
 mongo = PyMongo(app, uri=conn)
-
-#Defining the routes
 @app.route("/")
 def home():
     return render_template("index.html")
-    # return "Your  flask is running"
+
 
 @app.route("/api/v1.0/weather_data")
 def all_data():
     #entries=[]
     us_data = mongo.db.us.find()
+    #entries={"first":us_data}
+    # for d in us_data:
+    #     entries.append(json.loads(json_util.dumps(d)))
+    #    # entries.append(json.dumps(d, default=json_util.default))
+    #     entries.append(d)
+    #print(len(entries))
     return jsonify(json.loads(json_util.dumps(us_data)))
-  
-#client=pymongo.MongoClient(conn)
-
-#db=client.weather_data
-#us_data= db.us.find()
 
 @app.route("/api/v1.0/weather_data/<city_name>")
 def city_data(city_name):
